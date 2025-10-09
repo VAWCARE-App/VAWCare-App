03c4b455c057c85267fcfcb92d67814b5fe8df13
Kevin <jadenyuki486@gmail.com>
2025-10-09 18:23:43 +0800

    Add test rules endpoint to dssRoutes for rules engine evaluation

--- backend/src/routes/dssRoutes.js
+++ backend/src/routes/dssRoutes.js
@@ -5,6 +5,9 @@ const dssController = require('../controllers/dssController');
 // Public suggestion endpoint: POST /api/dss/suggest
 router.post('/suggest', dssController.suggest);
 
+// Test rules engine without running model: POST /api/dss/test
+router.post('/test', dssController.testRules);
+
 // Admin-only (or developer) training endpoint
 router.post('/train', dssController.trainModel);
 

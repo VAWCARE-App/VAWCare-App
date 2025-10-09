556528318cdaf447681bed9a8b5e36acacf514cb
Kevin <jadenyuki486@gmail.com>
2025-10-09 10:00:12 +0800

    Add route to list alerts

--- backend/src/routes/alertRoutes.js
+++ backend/src/routes/alertRoutes.js
@@ -5,4 +5,7 @@ const { resolveAlert } = require('../controllers/alertController');
 // Resolve alert (set status to Resolved)
 router.put('/:id/resolve', resolveAlert);
 
+// List alerts
+router.get('/', require('../controllers/alertController').listAlerts);
+
 module.exports = router;

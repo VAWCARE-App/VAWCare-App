0321559d751f4e86fbc2917a3f92e8ff9bece5b4
Kevin <jadenyuki486@gmail.com>
2025-11-19 18:28:11 +0800

    Add clearAll route to notificationRoutes for bulk deletion of notifications

--- backend/src/routes/notificationRoutes.js
+++ backend/src/routes/notificationRoutes.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const { getNotifications, deleteNotification } = require('../controllers/notificationController');
+const { getNotifications, deleteNotification, clearAll } = require('../controllers/notificationController');
 
 // Get all notifications
 router.get('/', getNotifications);
 
 // Delete a notification
 router.delete('/:id', deleteNotification);
+
+// Clear all notifications
+router.delete('/clear-all', clearAll);
 
 module.exports = router;

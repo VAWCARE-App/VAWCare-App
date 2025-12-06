4da173be8c50bd405b03b85edcf6cddd5282ae27
Rozvalle <elmovaleros11@gmail.com>
2025-12-06 09:21:06 +0800

    refactor: Simplify Vite PWA configuration and maintain 6MB cache file size limit

--- frontend/Dashboard/vite.config.js
+++ frontend/Dashboard/vite.config.js
@@ -3,22 +3,21 @@ import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
 import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     svgr(),
-    VitePWA(
-      {
-        registerType: 'autoUpdate',
-        includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
-        workbox: {
-          workbox: {
-            maximumFileSizeToCacheInBytes: 6 * 1024 * 1024, // 6 MB limit
-          }
-
-        },
+    VitePWA({
+      registerType: 'autoUpdate',
+      strategies: 'generateSW',   // required if using workbox options
+      includeAssets: [
+        'favicon.ico',
+        'robots.txt',
+        'apple-touch-icon.png'
+      ],
+      workbox: {
+        maximumFileSizeToCacheInBytes: 6 * 1024 * 1024 // 6MB
       }
-    )
-  ],
+    })
+  ]
 })
